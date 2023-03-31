682c17ab3fa8e85c707ffe3291c33786214434fe
TaherJalal <taherjalaljuma@gmail.com>
2023-03-31 20:06:31 +0300

    splide finally in place

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,14 @@
 import React , {useState} from 'react'
 import axios from 'axios'
 import { useQuery } from '@tanstack/react-query'
-import { Options, Splide, SplideSlide } from '@splidejs/react-splide';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/react-splide/css/core';
 
 function index() {
   const [trendingMovies ,setTrendingMovies] = useState([])
   const [topRatedMovies ,settopRatedMovies] = useState([])
   const [upcomingMovies ,setUpcomingMovies] = useState([])
   const [nowPlaying ,setnowPlaying] = useState([])
-
-  
-
-const options: Options = {
-  direction: 'ltr',
-};
-  
 
   const { isLoading, error, data } = useQuery({
     queryKey: ['landingPageData'],
@@ -27,33 +21,25 @@ const options: Options = {
         setnowPlaying(data.data[3].movies)
       },
   }) 
-
-  console.log(trendingMovies)
-
   if (isLoading) return 'Loading...'
 
   if (error) return 'An error has occurred: ' + error.message
 
   return (
-    <div className='px-3'>
-{trendingMovies.map((movie) => (
-  <Splide options={options.direction}>
-    <SplideSlide>
-    {movie?.original_title}
-  </SplideSlide> 
-  </Splide> 
- ))}      
- <br />
-
- {topRatedMovies.map((movie) => (
-  <Splide options={options.direction}>
-    <SplideSlide>
-    {movie?.original_title}
-  </SplideSlide> 
-  </Splide> 
- ))} 
+      <div className='px-3'>
+          <Splide options={{fixedWidth: "176px"}}>
+    {trendingMovies.map((movie) => (
+            <SplideSlide key={movie.id}>
+            <div className="flex flex-col w-44 h-52 bg-slate-300">
+              {movie.id}
+            </div>
+          </SplideSlide>
+        ))}
+        </Splide> 
     </div>
-  )
+    
+  );
 }
+
 
 export default index
