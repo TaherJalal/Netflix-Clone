b28ba830a4183578de49adb92c0e886c1f364813
TaherJalal <taherjalaljuma@gmail.com>
2023-04-01 20:59:32 +0300

    movies index no filter and search bar and pagination

--- pages/movies/index.tsx
+++ pages/movies/index.tsx
@@ -18,15 +18,17 @@ if (error) return 'An error has occurred: ' + error.message
 return (
   <>
     <Nav />
-    <div className='font-montserrat h-screen w-screen dark:bg-zinc-950 dark:text-white bg-white text-black grid gap-x-4 gap-y-4 gap-4 grid-cols-6 grid-rows-3 '>
+    <div className='font-montserrat pt-8 pl-20 dark:bg-zinc-950 dark:text-white bg-white text-black grid gap-4 grid-cols-4 grid-rows-6'>
     {
       data?.data.results.map((movie:any) => (
-        <div>
-          <Image src={movie.poster_path == null ? "public/next.svg" : movie.poster_path} width={500} height={500} alt="none" priority={true} />
-              <div className='flex flex-col gap-2 justify-center items-center text-white absolute inset-0 opacity-0 hover:opacity-100 transition-opacity duration-300 hover:bg-black hover:bg-opacity-60'>
-                <h6 className='text-sm'>{movie.original_title}</h6>
-                <p className='text-xs'>{movie.overview}</p>
-              </div>
+        <div className='relative'>
+          <Image src={"https://image.tmdb.org/t/p/original" + movie.poster_path == null ? "/next.svg" : "https://image.tmdb.org/t/p/original" + movie.poster_path} width={224} height={224} alt="none" priority={true} />
+
+          <div className='w-56 flex flex-col justify-center items-center absolute inset-0 opacity-0 hover:opacity-100 transition-opacity duration-300 hover:bg-black hover:bg-opacity-60'>
+              <h5 className='text-sm'>{movie.original_title}</h5>
+              <p className='text-xs'>{movie.overview}</p>
+          </div>
+
         </div>
       ))
     }
