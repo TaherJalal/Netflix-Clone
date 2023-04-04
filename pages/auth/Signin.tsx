518e63744ab53f0f1d2ca22bdc4dc9c8e4571d34
TaherJalal <taherjalaljuma@gmail.com>
2023-04-04 19:41:01 +0300

    change color

--- pages/auth/Signin.tsx
+++ pages/auth/Signin.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Signin() {
   return (
     <div className='font-montserrat'>
-      <section className="bg-gray-50 dark:bg-gray-900">
+      <section className=" dark:bg-zinc-950 dark:text-white bg-white text-black">
   <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
       <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
@@ -11,7 +11,7 @@ function Signin() {
                   Create an account
               </h1>
               <form className="space-y-4 md:space-y-6" action="#">
-                
+
               <div>
                       <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First Name</label>
                       <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required={true} />
