4ba97dab3846a26d29e2f4b6072c06b559290e49
TaherJalal <taherjalaljuma@gmail.com>
2023-04-05 16:00:21 +0300

    added signup page

--- pages/auth/Signin.tsx
+++ pages/auth/Signin.tsx
@@ -15,16 +15,6 @@ function Signin() {
               </h1>
               <form className="space-y-4 md:space-y-6" action="#">
 
-              <div>
-                      <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First Name</label>
-                      <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required={true} />
-                  </div>
-                  
-                  <div>
-                      <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last Name</label>
-                      <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required={true} />
-                  </div>
-
                   <div>
                       <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
                       <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required={true} />
@@ -38,7 +28,7 @@ function Signin() {
                   
                   <button type="submit" className="w-full text-white bg-cyan-500   font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Create an account</button>
                   <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                      Already have an account? <Link href="auth/Signin" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</Link>
+                      Don't have an account? <Link href="auth/Signin" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Create Account Here</Link>
                   </p>
               </form>
           </div>
