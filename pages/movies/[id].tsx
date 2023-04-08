195da653723434b0d4271928c0f2afe6dafe86ab
TaherJalal <taherjalaljuma@gmail.com>
2023-04-08 21:57:20 +0300

    moviue details not done

--- pages/movies/[id].tsx
+++ pages/movies/[id].tsx
@@ -3,23 +3,23 @@ import { useQuery } from '@tanstack/react-query'
 import Nav from '../Components/Nav'
 import axios from 'axios'
 import { useRouter } from 'next/router'
+import Image from 'next/image'
 
 function movieId() {
 
     const router = useRouter()
+
     const {id} = router.query
 
-    const [movies, setMovies] = useState([])
-
-    const {isLoading , error} = useQuery({
+    const {isLoading , error, data} = useQuery({
         queryKey:["movieData"],
-        queryFn: () => axios.get(`http://localhost:3000/api/movie/${id}`),
-        onSuccess(data) {
-            setMovies(data.data)
-        },
+        queryFn: () => axios.get(`http://localhost:3000/api/movie/${id}` , {
+            headers: {Authorization: localStorage.getItem("token")}
+        })
+        
     })
 
-    console.log(movies)
+    console.log(data)
 
     if (isLoading) return ( 
         <div className='dark:bg-zinc-950 dark:text-white bg-white text-black h-screen'>
@@ -33,8 +33,12 @@ function movieId() {
         )
 
   return (
-    <div>
+    <div className='overflow-hidden dark:bg-zinc-950 dark:text-white bg-white text-black'>
         <Nav />
+        <div>
+            <Image src={"https://image.tmdb.org/t/p/original" + data?.data.poster_path} width={600} height={600} alt='hhh' priority={true}/>
+            {/* <img src={"https://image.tmdb.org/t/p/original" + data?.data.poster_path} alt="" className='w-3/4 h-1/4'/> */}
+        </div>
     </div>
   )
 }
