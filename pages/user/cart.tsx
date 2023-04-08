68f77e48798cfcfaa00940e93c8dcc36d1af0233
TaherJalal <taherjalaljuma@gmail.com>
2023-04-08 16:42:59 +0300

    cart page not finished

--- pages/user/cart.tsx
+++ pages/user/cart.tsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React , {useState} from 'react'
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
 
 function cart() {
+  const [movie, setMovies] = useState<any>("")
+  const [balance, setBalance] = useState<number>()
+
+  const {isLoading, error} = useQuery({
+    queryKey: ["cartData"],
+    queryFn: () => axios.get("http://localhost:3000/api/cart" ,{
+      headers: {Authorization: localStorage.getItem("token")}
+    }),
+    onSuccess:(data) => {
+      setMovies(data.data.userCart)
+      setBalance(data.data.userbalance)
+    },
+  })
+
+  console.log(movie)
+
+  if (isLoading) return 'Loading...'
+
+  if (error) return 'An error has occurred: ' + error.message
+
   return (
-    <div>cart</div>
+    <div className='dark:bg-zinc-950 dark:text-white bg-white text-black h-screen'>
+d
+    </div>
   )
 }
 
