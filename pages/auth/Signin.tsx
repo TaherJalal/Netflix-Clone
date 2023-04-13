382a18c97da1e80beb714469b137cf10e135931d
TaherJalal <taherjalaljuma@gmail.com>
2023-04-13 16:24:02 +0300

    page reload after signin

--- pages/auth/Signin.tsx
+++ pages/auth/Signin.tsx
@@ -52,7 +52,7 @@ const post = async (e:any) => {
                   </div>
           
                   
-                  <button  className="w-full text-white bg-cyan-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Create an account</button>
+                  <button  className="w-full text-white bg-cyan-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign in</button>
                   <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                       Don't have an account? <Link href="Signup" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign-Up</Link>
                   </p>
